2ba4c0c82bc06e07ffcca2a725a72498948a44ff
Glauco Serrano <glaucoserrano@gmail.com>
2020-08-16 11:41:03 -0300

    chore: Altera retorno da rota

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const express= require('express');
 const app = express();
 
 app.get('/teste', (req,res)=> {
-   return res.json({hello:'world'})
+   return res.json({hello:'world 2'})
 });
-
 
 app.listen(3333);
